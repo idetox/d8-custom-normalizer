73f786362044868ff3cd6f5b44a426a27a5c92ca
elescot <elescot@ipsosenso.com>
2019-07-30 09:22:12 +0200

    Handle denormalization

--- src/Normalizer/NodeArticleNormalizer.php
+++ src/Normalizer/NodeArticleNormalizer.php
@@ -9,16 +9,43 @@ class NodeArticleNormalizer extends EntityNormalizer
 {
   protected $supportedInterfaceOrClass = Node::class;
 
-  public function supportsNormalization($data, $format = NULL) {
+  /**
+   * Returns TRUE if the normalizer can handle the request.
+   */
+  public function supportsNormalization($data, $format = NULL): bool
+  {
     return parent::supportsNormalization($data, $format) && 'json' === $format && 'article' === $data->getType();
   }
 
-  public function normalize($entity, $format = NULL, array $context = [])
+  /**
+   * Returns TRUE if the denormalizer can handle the request.
+   */
+  public function supportsDenormalization($data, $type, $format = NULL): bool
   {
-    $attributes = parent::normalize($entity,$format,$context);
+    return parent::supportsDenormalization($data, $type, $format);
+  }
+
+  /**
+   * Entity to Array
+   */
+  public function normalize($entity, $format = NULL, array $context = []): array
+  {
+    $attributes = parent::normalize($entity, $format, $context);
 
     // TODO alter attributes values
 
     return $attributes;
+  }
+
+  /**
+   * Array to Entity
+   */
+  public function denormalize($data, $class, $format = NULL, array $context = []): Node
+  {
+    $entity = parent::denormalize($data, $class, $format, $context);
+
+    // TODO alter Node object
+
+    return $entity;
   }
 }
